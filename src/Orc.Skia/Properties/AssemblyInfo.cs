7714318128b073481fb587513bbf810492e50693
Geert van Horrik <geert@catenalogic.com>
2018-03-16 15:28:47 +0100

    Migrate project structure

--- src/Orc.Skia/Properties/AssemblyInfo.cs
+++ src/Orc.Skia/Properties/AssemblyInfo.cs
@@ -7,7 +7,6 @@
 
 using System.Reflection;
 using System.Resources;
-using System.Runtime.InteropServices;
 
 // All other assembly info is defined in SharedAssembly.cs
 
@@ -15,9 +14,3 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Orc.Skia")]
 [assembly: AssemblyDescription("Orc.Skia library")]
 [assembly: NeutralResourcesLanguage("en-US")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-
-[assembly: ComVisible(false)]
