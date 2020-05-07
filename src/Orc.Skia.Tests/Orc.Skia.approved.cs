453e46acca0ff2e71bf69ba7aaf27f68bdcc3833
Geert van Horrik <geert@catenalogic.com>
2020-05-07 14:04:14 +0200

    Approve API changes

--- src/Orc.Skia.Tests/Orc.Skia.approved.cs
+++ src/Orc.Skia.Tests/Orc.Skia.approved.cs
@@ -1,15 +1,15 @@
-﻿[assembly: System.Resources.NeutralResourcesLanguageAttribute("en-US")]
-[assembly: System.Runtime.Versioning.TargetFrameworkAttribute(".NETFramework,Version=v4.6", FrameworkDisplayName=".NET Framework 4.6")]
-[assembly: System.Windows.Markup.XmlnsDefinitionAttribute("http://schemas.wildgums.com/orc/skia", "Orc.Skia")]
-[assembly: System.Windows.Markup.XmlnsPrefixAttribute("http://schemas.wildgums.com/orc/skia", "orcskia")]
-[assembly: System.Windows.ThemeInfoAttribute(System.Windows.ResourceDictionaryLocation.None, System.Windows.ResourceDictionaryLocation.SourceAssembly)]
-public class static ModuleInitializer
+﻿[assembly: System.Resources.NeutralResourcesLanguage("en-US")]
+[assembly: System.Runtime.Versioning.TargetFramework(".NETFramework,Version=v4.7", FrameworkDisplayName=".NET Framework 4.7")]
+[assembly: System.Windows.Markup.XmlnsDefinition("http://schemas.wildgums.com/orc/skia", "Orc.Skia")]
+[assembly: System.Windows.Markup.XmlnsPrefix("http://schemas.wildgums.com/orc/skia", "orcskia")]
+[assembly: System.Windows.ThemeInfo(System.Windows.ResourceDictionaryLocation.None, System.Windows.ResourceDictionaryLocation.SourceAssembly)]
+public static class ModuleInitializer
 {
     public static void Initialize() { }
 }
 namespace Orc.Skia
 {
-    public class static AlphaBlendingHelper
+    public static class AlphaBlendingHelper
     {
         public const byte FullyOpaqueAlpha = 255;
         public const byte FullyTransparentAlpha = 0;
@@ -29,12 +29,12 @@ namespace Orc.Skia
         public Crc32() { }
         public Crc32(uint polynomial, uint seed) { }
         public override int HashSize { get; }
-        public static uint Compute(byte[] buffer) { }
-        public static uint Compute(uint seed, byte[] buffer) { }
-        public static uint Compute(uint polynomial, uint seed, byte[] buffer) { }
         protected override void HashCore(byte[] array, int ibStart, int cbSize) { }
         protected override byte[] HashFinal() { }
         public override void Initialize() { }
+        public static uint Compute(byte[] buffer) { }
+        public static uint Compute(uint seed, byte[] buffer) { }
+        public static uint Compute(uint polynomial, uint seed, byte[] buffer) { }
     }
     public enum LineType
     {
@@ -46,8 +46,8 @@ namespace Orc.Skia
     {
         public Margin(double margin) { }
         public Margin(int margin) { }
-        public Margin(int top, int bottom, int left, int right) { }
         public Margin(double top, double bottom, double left, double right) { }
+        public Margin(int top, int bottom, int left, int right) { }
         public bool AddToStandard { get; set; }
         public int Bottom { get; set; }
         public bool IsZero { get; }
@@ -56,14 +56,14 @@ namespace Orc.Skia
         public int Top { get; set; }
         public static Orc.Skia.Margin CurrentOrNew(Orc.Skia.Margin margin, double value = 0) { }
     }
-    public class static MatrixExtensions
+    public static class MatrixExtensions
     {
         public static System.Windows.Media.Matrix Copy(this System.Windows.Media.Matrix matrix) { }
         public static System.Windows.Media.Matrix Multiply(this System.Windows.Media.Matrix matrix, System.Windows.Media.Matrix secondMatrix) { }
         public static System.Windows.Rect TransformRectangle(this System.Windows.Media.Matrix matrix, System.Windows.Rect rectangle) { }
     }
-    public class static PointExtensions { }
-    public class static RectangleExtensions
+    public static class PointExtensions { }
+    public static class RectangleExtensions
     {
         public static System.Windows.Point GetTopLeft(this System.Windows.Rect rect) { }
         public static bool IntersectsAndIsNotNeighbour(this System.Windows.Rect rect, System.Windows.Rect toCheck) { }
@@ -75,7 +75,7 @@ namespace Orc.Skia
         public static System.Windows.Rect SubtractWidth(this System.Windows.Rect rect, double value) { }
         public static System.Windows.Int32Rect ToInt32Rect(this System.Windows.Rect rect) { }
     }
-    public class static SKCanvasExtensions
+    public static class SKCanvasExtensions
     {
         public const float DefaultFontSize = 14F;
         public const float DefaultFontWidth = 4F;
@@ -83,14 +83,39 @@ namespace Orc.Skia
         public static void DrawHorizontalLine(this SkiaSharp.SKCanvas canvas, System.Windows.Point begin, System.Windows.Point end, SkiaSharp.SKPaint paint) { }
         public static void DrawLine(this SkiaSharp.SKCanvas canvas, System.Windows.Point begin, System.Windows.Point end, SkiaSharp.SKPaint paint) { }
         public static void DrawLineThroughPoints(this SkiaSharp.SKCanvas canvas, System.Collections.Generic.IEnumerable<System.Windows.Point> points, SkiaSharp.SKPaint paint) { }
-        public static void DrawText(this SkiaSharp.SKCanvas canvas, string line, System.Windows.Rect rect, System.Windows.Media.Color color, float fontSize = 14, double width = 4, System.Nullable<double> lineSpacing = null, SkiaSharp.SKTextAlign textAlign = 0, bool clip = True) { }
-        public static void DrawText(this SkiaSharp.SKCanvas canvas, string[] lines, System.Windows.Rect rect, System.Windows.Media.Color color, float fontSize = 14, double width = 4, System.Nullable<double> lineSpacing = null, SkiaSharp.SKTextAlign textAlign = 0, bool clip = True) { }
+        public static void DrawText(this SkiaSharp.SKCanvas canvas, string line, System.Windows.Rect rect, System.Windows.Media.Color color, float fontSize = 14, double width = 4, double? lineSpacing = default, SkiaSharp.SKTextAlign textAlign = 0, bool clip = true) { }
+        public static void DrawText(this SkiaSharp.SKCanvas canvas, string[] lines, System.Windows.Rect rect, System.Windows.Media.Color color, float fontSize = 14, double width = 4, double? lineSpacing = default, SkiaSharp.SKTextAlign textAlign = 0, bool clip = true) { }
         public static void DrawVerticalLine(this SkiaSharp.SKCanvas canvas, System.Windows.Point begin, System.Windows.Point end, SkiaSharp.SKPaint paint) { }
         public static System.Windows.Rect MeasureTextBounds(this SkiaSharp.SKCanvas canvas, string text, System.Windows.Media.Color color, float fontSize = 14, double width = 4, SkiaSharp.SKTextAlign textAlign = 0) { }
     }
-    public class static SKColorExtensions
+    public static class SKColorExtensions
     {
         public static SkiaSharp.SKColor ToSKColor(this System.Windows.Media.Color color) { }
+    }
+    public static class SKMatrixExtensions { }
+    public static class SKPaintHelper
+    {
+        public static SkiaSharp.SKPaint CreateLinePaint(double width, System.Windows.Media.Color color, Orc.Skia.LineType lineType = 0) { }
+        public static SkiaSharp.SKPaint CreateTextPaint(double fontSize, double width, System.Windows.Media.Color color, SkiaSharp.SKTextAlign textAlign = 0) { }
+    }
+    public static class SKPointExtensions
+    {
+        public static System.Windows.Point ToPoint(this SkiaSharp.SKPoint point) { }
+        public static System.Windows.Point ToPoint(this SkiaSharp.SKPointI point) { }
+        public static SkiaSharp.SKPoint ToSKPoint(this System.Windows.Point point) { }
+    }
+    public static class SKRectExtensions
+    {
+        public static System.Windows.Int32Rect ToInt32Rect(this SkiaSharp.SKRectI rect) { }
+        public static System.Windows.Rect ToRect(this SkiaSharp.SKRect rect) { }
+        public static System.Windows.Rect ToRect(this SkiaSharp.SKRectI rect) { }
+        public static SkiaSharp.SKRect ToSKRect(this System.Windows.Rect rect) { }
+        public static SkiaSharp.SKRectI ToSKRectI(this System.Windows.Rect rect) { }
+    }
+    public static class SKSizeExtensions
+    {
+        public static System.Windows.Size ToSize(this SkiaSharp.SKSize size) { }
+        public static System.Windows.Size ToSize(this SkiaSharp.SKSizeI size) { }
     }
     public class SkiaCanvas : System.Windows.Controls.Canvas
     {
@@ -114,37 +139,12 @@ namespace Orc.Skia
         protected virtual void Terminate() { }
         public void Update() { }
     }
-    public class static SkiaEnvironment { }
+    public static class SkiaEnvironment { }
     public class SkiaException : System.Exception
     {
         public SkiaException(string message) { }
     }
-    public class static SKMatrixExtensions { }
-    public class static SKPaintHelper
-    {
-        public static SkiaSharp.SKPaint CreateLinePaint(double width, System.Windows.Media.Color color, Orc.Skia.LineType lineType = 0) { }
-        public static SkiaSharp.SKPaint CreateTextPaint(double fontSize, double width, System.Windows.Media.Color color, SkiaSharp.SKTextAlign textAlign = 0) { }
-    }
-    public class static SKPointExtensions
-    {
-        public static System.Windows.Point ToPoint(this SkiaSharp.SKPointI point) { }
-        public static System.Windows.Point ToPoint(this SkiaSharp.SKPoint point) { }
-        public static SkiaSharp.SKPoint ToSKPoint(this System.Windows.Point point) { }
-    }
-    public class static SKRectExtensions
-    {
-        public static System.Windows.Int32Rect ToInt32Rect(this SkiaSharp.SKRectI rect) { }
-        public static System.Windows.Rect ToRect(this SkiaSharp.SKRectI rect) { }
-        public static System.Windows.Rect ToRect(this SkiaSharp.SKRect rect) { }
-        public static SkiaSharp.SKRect ToSKRect(this System.Windows.Rect rect) { }
-        public static SkiaSharp.SKRectI ToSKRectI(this System.Windows.Rect rect) { }
-    }
-    public class static SKSizeExtensions
-    {
-        public static System.Windows.Size ToSize(this SkiaSharp.SKSizeI size) { }
-        public static System.Windows.Size ToSize(this SkiaSharp.SKSize size) { }
-    }
-    public class static WriteableBitmapExtensions { }
+    public static class WriteableBitmapExtensions { }
 }
 namespace Orc.Skia.Coloring
 {
@@ -170,10 +170,16 @@ namespace Orc.Skia.Coloring
         public double Saturation { get; set; }
         public override bool Equals(object obj) { }
         public override int GetHashCode() { }
-        public static bool ==(Orc.Skia.Coloring.ColorHsb item1, Orc.Skia.Coloring.ColorHsb item2) { }
         public static bool !=(Orc.Skia.Coloring.ColorHsb item1, Orc.Skia.Coloring.ColorHsb item2) { }
+        public static bool ==(Orc.Skia.Coloring.ColorHsb item1, Orc.Skia.Coloring.ColorHsb item2) { }
     }
-    public class static ColorsExtensions
+    public enum ColorShade
+    {
+        Light = 0,
+        Medium = 1,
+        Dark = 2,
+    }
+    public static class ColorsExtensions
     {
         public static int AlphaBlend(this System.Windows.Media.Color source, int dest) { }
         public static float GetPerceptiveLuminance(this System.Windows.Media.Color color) { }
@@ -188,17 +194,11 @@ namespace Orc.Skia.Coloring
         public static int ToInt(this System.Windows.Media.Color color) { }
         public static int ToInt(this System.Windows.Media.Color color, Orc.Skia.Coloring.ColorShade colorShade) { }
     }
-    public enum ColorShade
-    {
-        Light = 0,
-        Medium = 1,
-        Dark = 2,
-    }
     public interface IColorGenerator
     {
         System.Windows.Media.Color Generate(object value, string salt = null);
     }
-    public class static IntColors
+    public static class IntColors
     {
         public static readonly int Black;
         public static readonly int Blue;
