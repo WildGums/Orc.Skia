ca8f7dc35df4038c0f78c09c37ba0af656b5eebe
Geert van Horrik <geert@catenalogic.com>
2020-05-08 20:17:46 +0200

    Add support for .NET Core 3.1

--- src/Orc.Skia.Tests/GlobalInitialization.approvaltests.cs
+++ src/Orc.Skia.Tests/GlobalInitialization.approvaltests.cs
@@ -12,19 +12,3 @@ using ApprovalTests.Reporters;
 #else
 [assembly: UseReporter(typeof(DiffReporter))]
 #endif
-
-public static class TargetFrameworkResolver
-{
-    public const string Current =
-
-#if NET45
-            "NET45"
-#elif NET46
-            "NET46"
-#elif NET47
-            "NET47"
-#else
-            "Unknown"
-#endif
-        ;
-}
