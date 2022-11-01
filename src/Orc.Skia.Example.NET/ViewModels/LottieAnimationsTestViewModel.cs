047de716429517715a0a27edc5ecb28ca1d2fc5f
Artem <art.saikin@gmail.com>
2022-11-02 02:45:32 +0300

    Fix example

--- src/Orc.Skia.Example.NET/ViewModels/LottieAnimationsTestViewModel.cs
+++ src/Orc.Skia.Example.NET/ViewModels/LottieAnimationsTestViewModel.cs
@@ -10,41 +10,13 @@
 
     internal sealed class LottieAnimationsTestViewModel : ViewModelBase
     {
-        private readonly IFileService _fileService;
-        private readonly IMessageService _messageService;
-
-        public LottieAnimationsTestViewModel(IFileService fileService, IMessageService messageService)
+        public LottieAnimationsTestViewModel()
         {
-            Argument.IsNotNull(() => fileService);
-            Argument.IsNotNull(() => messageService);
 
-            _fileService = fileService;
-            _messageService = messageService;
         }
 
         public string SelectedFile { get; set; }
 
-        public string Version { get; private set; }
-        public TimeSpan Duration { get; private set; }
-        public double Fps { get; private set; }
-
         public Animation AnimationInPlayback { get; set; }
-
-        private void OnSelectedFileChanged()
-        {
-            //if (SkiaSharp.Skottie.Animation.TryCreate(skiaStream, out var animation))
-            //{
-            //    Version = animation.Version;
-            //    Duration = animation.Duration;
-            //    Fps = animation.Fps;
-            //}
-            //else
-            //{
-            //    _messageService.ShowErrorAsync("File content cannot be recognized as Lottie format");
-            //    return;
-            //}
-
-            //AnimatedCanvas.SetAnimation(animation);
-        }
     }
 }
