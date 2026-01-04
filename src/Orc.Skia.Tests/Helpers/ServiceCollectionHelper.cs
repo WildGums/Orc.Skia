c48f0734127f8d93209442996ffcaa9f4e4b068e
Geert van Horrik <geert@catenalogic.com>
2026-01-05 00:44:10 +0100

    Update

---
+++ src/Orc.Skia.Tests/Helpers/ServiceCollectionHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace Orc.Skia.Tests
+{
+    using Catel;
+    using Microsoft.Extensions.DependencyInjection;
+    using Orc.Skia;
+
+    internal static class ServiceCollectionHelper
+    {
+        public static IServiceCollection CreateServiceCollection()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddLogging();
+            serviceCollection.AddCatelCore();
+            serviceCollection.AddOrcSkia();
+
+            return serviceCollection;
+        }
+    }
+}
